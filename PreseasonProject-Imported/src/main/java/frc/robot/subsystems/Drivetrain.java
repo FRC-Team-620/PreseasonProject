0f12bfd97774e6791638db4d93656563c5fbae02
tjamesw123 <tweinhardt@verizon.net>
2022-11-30 15:24:34 -0500

    Teaching base--setup Drivetrain

--- PreseasonProject-Imported/src/main/java/frc/robot/subsystems/Drivetrain.java
+++ PreseasonProject-Imported/src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -17,95 +17,39 @@ public class Drivetrain extends SubsystemBase {
   private CANSparkMax rightFrontMotor = new CANSparkMax(2, MotorType.kBrushless);
   private CANSparkMax leftRearMotor = new CANSparkMax(3, MotorType.kBrushless);
   private CANSparkMax leftFrontMotor = new CANSparkMax(4, MotorType.kBrushless);
-  
-  private DifferentialDrive differentialDrive;
 
   public Drivetrain() {
-    setupMotors();
-    setupFollowerMotors();
-
-/*     leftFrontMotor.setInverted(true);
- */
-    //creates differential drive
-    differentialDrive = new DifferentialDrive(leftFrontMotor, rightFrontMotor);
-  }
-
-  private void setupMotors() {
-    rightRearMotor = setupMotor(rightRearMotor);
-    rightFrontMotor = setupMotor(rightFrontMotor);
-    leftRearMotor = setupMotor(leftRearMotor);
-    leftFrontMotor = setupMotor(leftFrontMotor);
-  }
-
-  public void setupFollowerMotors() {
-    //leftRearMotor.follow(leftFrontMotor);
-    //rightRearMotor.follow(rightFrontMotor);
-
-    rightFrontMotor.setInverted(false);
-    leftFrontMotor.setInverted(false);
-
-    //rightRearMotor.follow(rightFrontMotor);
-    // leftRearMotor.follow(leftFrontMotor);
-  }
-
-
-
-  public CANSparkMax setupMotor(CANSparkMax motor) {
     //Double passed into this method sets the max speed the motor can accelrate in a second 
-    motor.setOpenLoopRampRate(0.2);
-
+    rightRearMotor.setOpenLoopRampRate(0.2);
     //Int passed into the method sets the current limit for the motor
-    motor.setSmartCurrentLimit(60);
-
+    rightRearMotor.setSmartCurrentLimit(60);
     //Can set the mode to either brake or coast
-    motor.setIdleMode(IdleMode.kBrake);//This method passes an enum that represents brake 
+    rightRearMotor.setIdleMode(IdleMode.kBrake);//This method passes an enum that represents brake 
+    
+    //Double passed into this method sets the max speed the motor can accelrate in a second 
+    rightFrontMotor.setOpenLoopRampRate(0.2);
+    //Int passed into the method sets the current limit for the motor
+    rightFrontMotor.setSmartCurrentLimit(60);
+    //Can set the mode to either brake or coast
+    rightFrontMotor.setIdleMode(IdleMode.kBrake);//This method passes an enum that represents brake 
 
-    System.out.println("Motor created: " + motor.toString());
-    return motor;
-  }
+    //Double passed into this method sets the max speed the motor can accelrate in a second 
+    leftRearMotor.setOpenLoopRampRate(0.2);
+    //Int passed into the method sets the current limit for the motor
+    leftRearMotor.setSmartCurrentLimit(60);
+    //Can set the mode to either brake or coast
+    leftRearMotor.setIdleMode(IdleMode.kBrake);//This method passes an enum that represents brake 
 
-  //Used as a method to pass through the infomation from the drive command (Like the speed, rotation, and if we want quick turn (Tank drive) or not)
-  //into the differentialDrive in this subsystem
-  public void setCurvatureDrive(double speed, double rotation, boolean quickTurn) {
-    differentialDrive.curvatureDrive(speed, rotation, quickTurn);
-  }
-
-  public void setRightMotors(double speed) {
-    this.rightFrontMotor.set(speed);
-  }
-
-  public void setLeftMotors(double speed) {
-    this.leftFrontMotor.set(speed);
+    //Double passed into this method sets the max speed the motor can accelrate in a second 
+    leftFrontMotor.setOpenLoopRampRate(0.2);
+    //Int passed into the method sets the current limit for the motor
+    leftFrontMotor.setSmartCurrentLimit(60);
+    //Can set the mode to either brake or coast
+    leftFrontMotor.setIdleMode(IdleMode.kBrake);//This method passes an enum that represents brake 
   }
 
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
-    System.out.println("RF: " + rightFrontMotor.get());
-    System.out.println("RB: " + rightRearMotor.get());
-    System.out.println("LF: " + leftFrontMotor.get());
-    System.out.println("LB: " + leftRearMotor.get());
-    System.out.println();
-  }
 
-  @Override
-  public void simulationPeriodic() {
-    // This method will be called once per scheduler run during simulation
-  }
-
-  public CANSparkMax getRightRearMotor() {
-    return rightRearMotor;
-  }
-
-  public CANSparkMax getRightFrontMotor() {
-    return rightFrontMotor;
-  }
-
-  public CANSparkMax getLeftRearMotor() {
-    return leftRearMotor;
-  }
-  
-  public CANSparkMax getLeftFrontMotor() {
-    return leftFrontMotor;
   }
 }
